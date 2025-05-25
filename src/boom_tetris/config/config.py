dc5053d72ef045569526bfd020f6e2c564d71e2f
Auke <afbruinsma@gmail.com>
2025-05-25 10:28:26 +0200

    Updated

--- src/boom_tetris/config/config.py
+++ src/boom_tetris/config/config.py
@@ -33,15 +33,16 @@ class Config:
         config_dict["BOARD"]["CELL"] = {"WIDTH": cell_width, "HEIGHT": cell_height}
 
         return config_dict
-    
-    def _construct_augmented_config_path(self, file_path: str, infix: str="_augmented") -> str:
+
+    def _construct_augmented_config_path(
+        self, file_path: str, infix: str = "_augmented"
+    ) -> str:
         """ """
         augmented_file_path = file_path.with_name(
             f"{file_path.stem}{infix}{self.config_path.suffix}"
         )
 
         return augmented_file_path
-
 
     def _save_config(self, file_path: str, config: dict) -> None:
         """ """
@@ -51,9 +52,13 @@ class Config:
     def augment_config(self) -> ConfigModel:
         """ """
         augmented_config: dict = self._add_parameters(config=self.config)
-        augmented_config_path = self._construct_augmented_config_path(file_path=self.config_path)
+        augmented_config_path = self._construct_augmented_config_path(
+            file_path=self.config_path
+        )
 
         self._save_config(file_path=augmented_config_path, config=augmented_config)
-        augmented_config: ConfigModel = self.load_config(file_path=augmented_config_path)
+        augmented_config: ConfigModel = self.load_config(
+            file_path=augmented_config_path
+        )
 
         return augmented_config
